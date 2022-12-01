4788fe8df248cbc0ae4875fe6dd78b179eb617df
Micah Ban <mtban@asu.edu>
2022-11-30 23:13:51 -0700

    Update front_end.js

--- front_end.js
+++ front_end.js
@@ -23,8 +23,27 @@ const bdate = document.getElementById('bdate');
 const bplace = document.getElementById('bplace');
 const debut = document.getElementById('debut');
 function setPlayerInfo(){
-    pname.innerText = "Player: " + document.getElementById('searchPlayer').value;
-    bdate.innerText = "Birthdate: 07/05/1994";
-    bplace.innerText = "Birthplace: Oshu, Japan";
-    debut.innerText = "Debut Year: 2018";
+    const myText = document.getElementById('searchPlayer').value;
+    if (myText === "Shohei Otani"){
+        pname.innerText = "Player: " + myText;
+        bdate.innerText = "Birthdate: 07/05/1994";
+        bplace.innerText = "Birthplace: Oshu, Japan";
+        debut.innerText = "Debut Year: 2018";
+    }
+    else if (myText === "Aaron Judge"){
+        pname.innerText = "Player: " + myText;
+        bdate.innerText = "Birthdate: 04/26/1992";
+        bplace.innerText = "Birthplace: Sacramento, California";
+        debut.innerText = "Debut Year: 2016";
+    }
+    else if (myText === "Trea Turner"){
+        pname.innerText = "Player: " + myText;
+        bdate.innerText = "Birthdate: 06/30/1993";
+        bplace.innerText = "Birthplace: Boynton Beach, Florida";
+        debut.innerText = "Debut Year: 2015";
+    }
+    else {
+        pname.innerText = "Player is not currently active.";
+    }
+
 }
