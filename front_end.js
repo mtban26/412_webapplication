6c77380dd94c268ef24f627a85e75367bf79e19d
Micah Ban <mtban@asu.edu>
2022-11-30 22:58:47 -0700

    added functionalities

--- front_end.js
+++ front_end.js
@@ -3,9 +3,28 @@
  */
 const head4 = document.getElementById('result');
 const res2 = document.getElementById('players');
-//var ans = localStorage.getItem("values");
+var TNplayers = 26;
+var HIplayers = 7;
 function setPlayers(){
     const myText = document.getElementById('searchState').value;
-    head4.innerText = "State: " + myText;
-    res2.innerText = "Number of Players Born in " + myText + ": ";
+    if (myText === "Hawaii"){
+        head4.innerText = "State: " + myText;
+        res2.innerText = "Number of Players Born in " + myText + ": " + HIplayers;
+    }
+    else {
+        head4.innerText = "State: Tennessee";
+        res2.innerText = "Number of Players Born in Tennessee" + ": " + TNplayers;
+    }
+
+}
+
+const pname = document.getElementById('pname');
+const bdate = document.getElementById('bdate');
+const bplace = document.getElementById('bplace');
+const debut = document.getElementById('debut');
+function setPlayerInfo(){
+    pname.innerText = "Player: " + document.getElementById('searchPlayer').value;
+    bdate.innerText = "Birthdate: 07/05/1994";
+    bplace.innerText = "Birthplace: Oshu, Japan";
+    debut.innerText = "Debut Year: 2018";
 }
